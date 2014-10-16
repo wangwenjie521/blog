3858af9a7d6ac58f2ca4ac946b4f00462ff4678d
wenjie <1662484899@qq.com>
2014-10-16 16:36:46 +0800

    big commit
    大更新

--- javascripts/sea_config.js
+++ javascripts/sea_config.js
@@ -3,41 +3,35 @@ seajs.config({
 	alias: {
 		'jquery' : 'jquery',
 		'gotop':'jquery.gotop',
-		'company':'company',
 		'dialog':'dialog',
-		'modernizr':'modernizr.custom'
+		'modernizr':'modernizr.custom',
+		'blog_index':'index',
+		'company':'kuaidi/company',
+		'kuaidi_index':"kuaidi/index_mask",
+		'kuaidi_company':"kuaidi/index_company"
 	},
 	map: [
-
 		[ /^(.*\.(?:css|js|tpl))(.*)$/i, '$1?'+20141009001 ]
 	],
-
 	// 路径配置
 	//paths: {
 	//	'gallery': 'https://a.alipayobjects.com/gallery'
 	//},
-
 	// 变量配置
 	//vars: {
 	//	'locale': 'zh-cn'
 	//},
-
 	// 映射配置
 	//map: [
 	//	['http://example.com/js/app/', 'http://localhost/js/app/']
 	//],
-
 	// 预加载项
-
 	preload: ['jquery','modernizr'],
 	//preload: ['jquery', 'seajs-text', 'seajs-combo'],
-
 	// 调试模式
 	debug: true,
-
 	// Sea.js 的基础路径
 	//base: 'seajs',
-
 	// 文件编码
 	charset: 'utf-8'
 });
